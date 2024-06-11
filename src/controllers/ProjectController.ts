277fa5056030f6f91cf30fbf100b9cb5fc9ad3ed
Valenmar03 <valenmrtnz8@gmail.com>
2024-06-11 14:49:54 -0300

    Create projects

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -1,6 +1,24 @@
+import colors from 'colors'
+
 import type { Request, Response } from 'express'
+import Project from '../models/Project'
 
 export class ProjectController {
+
+    static createProject = async (req: Request, res: Response) => {
+
+        const project = new Project(req.body)
+
+        try {
+            await project.save()
+            res.send('Project created succesfully')
+        } catch (error) {
+            console.log(colors.red('Error creating project...'))
+            console.log(error)
+            res.status(400).send({error: 'Error creating project', message: error.message})
+        }
+    }
+
     static getAllProjects = async (req: Request, res: Response) => {
         res.send('Todos los proyectos')
     }
--- src/server.ts
+++ src/server.ts
@@ -10,6 +10,8 @@ connectDB()
 
 const app = express();
 
+app.use(express.json())
+
 app.use('/api/projects', projectRoutes)
 
 export default app
