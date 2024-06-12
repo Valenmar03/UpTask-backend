2b42cdab42b20b11bc26e4c3137ae58abe211510
Valenmar03 <valenmrtnz8@gmail.com>
2024-06-12 08:24:20 -0300

    Connect Task and Project models

--- src/models/Project.ts
+++ src/models/Project.ts
@@ -1,9 +1,11 @@
-import mongoose, {Schema, Document} from "mongoose";
+import mongoose, {Schema, Document, PopulatedDoc, Types} from "mongoose";
+import { TaskType } from "./Task";
 
 export type ProjectType = Document & {
     projectName: string;
     clientName: string;
     description: string;
+    tasks:  PopulatedDoc<TaskType & Document>[]
 }
 
 const ProjectSchema : Schema = new Schema({
@@ -22,7 +24,13 @@ const ProjectSchema : Schema = new Schema({
         required: true,
         trim: true,
     },
-})
+    tasks: [
+        {
+            type: Types.ObjectId,
+            ref: 'Task'
+        }
+    ]
+}, {timestamps: true});
 
 const Project = mongoose.model<ProjectType>("Project", ProjectSchema)
 export default Project
--- src/models/Task.ts
+++ src/models/Task.ts
@@ -1,8 +1,20 @@
-import mongoose, {Schema, Document} from "mongoose";
+import mongoose, {Schema, Document, Types} from "mongoose";
+
+const taskStatus = {
+    PENDING: 'pending',
+    ON_HOLD: 'onHold',
+    IN_PROGRESS: 'inProgress',
+    UNDER_REVIEW: 'underReview',
+    COMPLETED: 'completed'
+} as const;
+
+export type TaskStatus = typeof taskStatus[keyof typeof taskStatus]
 
 export type TaskType = Document & {
     name: string;
     description: string;
+    project: Types.ObjectId;
+    status: TaskStatus;
 }
 
 export const TaskSchema : Schema = new Schema({
@@ -15,7 +27,16 @@ export const TaskSchema : Schema = new Schema({
         type: String, 
         trim: true,
         required: true
+    },
+    project: {
+        type: Types.ObjectId,
+        ref: 'Project'
+    },
+    status: {
+        type: String,
+        enum: Object.values(taskStatus),
+        default: taskStatus.PENDING
     }
-})
+}, {timestamps: true})
 const Task = mongoose.model<TaskType>('Task', TaskSchema)
 export default Task
