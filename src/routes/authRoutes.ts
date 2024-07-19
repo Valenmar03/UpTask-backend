4c44ec85b24c1cc5287fff956679c517d398e229
Valenmar03 <valenmrtnz8@gmail.com>
2024-07-19 11:07:28 -0300

    Validacion en la creacion de cuentas

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,9 +1,26 @@
 import { Router } from "express";
+import { body } from "express-validator";
+import { AuthController } from "../controllers/AuthController";
+import { handleInputErrors } from "../middleware/validation";
 
 const router = Router()
 
-router.get('/', (req, res)  => {
-    res.send('Desde authRoutes...')
-})
+router.post('/create-account',
+    body('name')
+        .notEmpty().withMessage('Account name is required'),
+    body('email')
+        .notEmpty().withMessage('Email is required'),
+    body('password')
+        .isLength({min: 8}).withMessage('The Password is to short, must have at least 8 characters'),
+    body('password_confirmation')
+        .custom((value, {req}) => {
+            if(value !== req.body.password){
+                throw new Error('The passwords do not match')
+            }
+            return true
+        }),
+    handleInputErrors,
+    AuthController.createAccount
+)
 
 export default router
