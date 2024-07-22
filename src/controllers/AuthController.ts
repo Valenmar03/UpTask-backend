145af7cef9965562c8d4764e4bd2387eb9c78a71
Valenmar03 <valenmrtnz8@gmail.com>
2024-07-22 12:18:42 -0300

    Confirmar cuenta con token y eliminar token

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -37,4 +37,25 @@ export class AuthController {
             res.status(500).send({error: 'Hubo un error'})
         }
     }
+
+    static confirmAccount = async (req: Request, res: Response) => {
+        try {
+            const {token} = req.body
+            const tokenExists = await Token.findOne({token})
+
+            if(!tokenExists){
+                const error = new Error('Invalid Token') 
+                return res.status(409).send({error: error.message})
+            }
+
+            const user = await User.findById(tokenExists.user)
+            user.confirmed = true
+            
+            await Promise.allSettled([user.save(), tokenExists.deleteOne()])
+            
+            res.send('Account confirmed')
+        } catch (error) {
+            res.status(500).send({error: error.message})
+        }
+    }
 }
