bfee0d4b8bc40715f27c09b80751e8cf986b1735
Valenmar03 <valenmrtnz8@gmail.com>
2024-07-22 08:42:05 -0300

    Guardo user y hasheo password

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,7 +1,17 @@
 import type { Request, Response } from "express"
+import bcrypt from 'bcrypt'
+import User from "../models/Auth"
+import { hashPassword } from "../utils/auth"
 
 export class AuthController {
     static createAccount = async (req: Request, res: Response) => {
-        res.send('Desde authRoutes...!°')
+        try {
+            const user = new User(req.body)
+            user.password = await hashPassword(req.body.password)
+            await user.save()
+            res.send('Cuenta creada, revisa tu email para confirmarla')
+        } catch(error) {
+            res.status(500).send({error: 'Hubo un error'})
+        }
     }
 }
