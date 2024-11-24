22807ced4a468182369583ef6b9188aabc4fded3
Valenmar03 <valenmrtnz8@gmail.com>
2024-11-23 21:08:03 -0300

    Elimnando tares y sus notas cuando se elimina un proyecto

--- src/models/Project.ts
+++ src/models/Project.ts
@@ -1,6 +1,7 @@
 import mongoose, { Schema, Document, PopulatedDoc, Types } from "mongoose";
-import { TaskType } from "./Task";
+import Task, { TaskType } from "./Task";
 import { UserType } from "./Auth";
+import Note from "./Note";
 
 export type ProjectType = Document & {
    projectName: string;
@@ -47,6 +48,18 @@ const ProjectSchema: Schema = new Schema(
    },
    { timestamps: true }
 );
+
+//Middleware
+ProjectSchema.pre('deleteOne', {document: true}, async function(){
+   const projectId = this._id
+   if(!projectId) return 
+
+   const tasks = await Task.find({ project: projectId})
+   for(const task of tasks) {
+      await Note.deleteMany({ task: task.id })
+   }
+   await Task.deleteMany({project: projectId})
+})
 
 const Project = mongoose.model<ProjectType>("Project", ProjectSchema);
 export default Project;
